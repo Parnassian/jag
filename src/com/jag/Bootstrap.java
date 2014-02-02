5383515ac98050d955c04bfa8cd006ddf24bf065
Parnassian <parnassian@pbot.org>
2014-02-02 21:20:54 +0100

    Updates

--- src/com/jag/Bootstrap.java
+++ src/com/jag/Bootstrap.java
@@ -1,5 +1,8 @@
 package com.jag;
 
+import javax.swing.JOptionPane;
+import javax.swing.UIManager;
+
 /**
  * 
  * @author Parnassian
@@ -7,12 +10,37 @@ package com.jag;
  * @since 0.1
  * @see jag
  * @category rs3
- *
+ * 
  */
 public class Bootstrap {
 
 	public static void main(String[] args) {
-	
+		handleArgs(args);
+		setLookAndFeel();
+	}
+
+	private static void handleArgs(String[] args) {
+		// just an example...
+		for (int i = 0; i < args.length; i++) {
+			String arg = args[i];
+			switch (arg) {
+			case "-exit":
+				System.exit(0);
+				break;
+			case "-msgbox":
+				JOptionPane.showMessageDialog(null, args[++i], "Welcome",
+						JOptionPane.INFORMATION_MESSAGE);
+				break;
+			}
+		}
+	}
+
+	private static void setLookAndFeel() {
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Throwable t) {
+			t.printStackTrace();
+		}
 	}
 
 }
---
+++ src/com/jag/ui/JagUI.java
@@ -0,0 +1,14 @@
+package com.jag.ui;
+
+import javax.swing.JFrame;
+
+public class JagUI extends JFrame {
+	private static final long serialVersionUID = 808838952211814951L;
+
+	public JagUI() {
+		setTitle("Jag");
+		
+		pack();
+	}
+
+}
