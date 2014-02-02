6154c25e09ba8df8d300dfcb3dac56a095480d85
Parnassian <parnassian@pbot.org>
2014-02-02 21:12:17 +0100

    Bootstrap v0.1

--- src/com/jag/Bootstrap.java
+++ src/com/jag/Bootstrap.java
@@ -1,9 +1,18 @@
 package com.jag;
 
+/**
+ * 
+ * @author Parnassian
+ * @version 0.1
+ * @since 0.1
+ * @see jag
+ * @category rs3
+ *
+ */
 public class Bootstrap {
 
 	public static void main(String[] args) {
-		
+	
 	}
 
 }
